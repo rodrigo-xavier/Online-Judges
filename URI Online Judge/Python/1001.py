2ff350083c8942e90807094d31477321d3756040
rodrigo-xavier <rodrigomatiasxavier@gmail.com>
2018-02-04 16:58:33 -0200

    commit inicial python

---
+++ URI Online Judge/Python/1001.py
@@ -0,0 +1,14 @@
+""" 
+Problem: 1001 - Extremely Basic
+ 
+The problem has been resolved using the sum operation, 
+native to language Python.
+
+"""
+
+integer_A = int(input())
+integer_B = int(input())
+
+sum_result = integer_A + integer_B
+
+print("X =", sum_result)
